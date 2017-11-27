2850d1405595fc1784a2c94e9b3af69c9585016c
Nico Madysa <nico.madysa@tu-dresden.de>
2017-11-28 00:30:04 +0100

    Add more information to setup.py.

--- setup.py
+++ setup.py
@@ -18,6 +18,13 @@
 from setuptools import setup, find_packages
 
 
+long_description = """Headercount goes through a C or C++ project and
+searches for #include directives. It recursively follows project header
+includes and prints statistics about which header was included how many
+times. This indicates which headers are depended on the most, which is
+useful when debugging long compile times of medium-sized projects.
+"""
+
 setup(
     name='headercount',
     version='0.1',
@@ -32,7 +39,24 @@ setup(
     author='Nico Madysa',
     author_email='uebertreiber@gmx.de',
     description='Count directly and indirectly included headers in a C/C++ project',
+    long_description=long_description,
     license='Apache License, Version 2.0',
-    keywords='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Debuggers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        ],
+    keywords='header include c c++ include',
     url='https://github.com/troiganto/headercount',
 )
