a436239c85ed928f3797d0b1f940d12ad5b1bcf9
Nico Madysa <nico.madysa@tu-dresden.de>
2017-11-28 00:03:31 +0100

    Add license.

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Nico Madysa
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from setuptools import setup, find_packages
 
@@ -17,7 +32,7 @@ setup(
     author='Nico Madysa',
     author_email='uebertreiber@gmx.de',
     description='Count directly and indirectly included headers in a C/C++ project',
-    license='',
+    license='Apache License, Version 2.0',
     keywords='',
     url='https://github.com/troiganto/headercount',
 )
